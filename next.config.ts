525616931db10ba5ac51a3374b1fbeea19449b99
colton615 <colton615@gmail.com>
2025-11-11 12:20:50 +0000

    ok

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
